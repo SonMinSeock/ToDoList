eff5d2e732d7758f9724cbd76ef51cbc2ef845eb
SonMinSeock <44064257+SonMinSeock@users.noreply.github.com>
2022-09-16 10:04:03 +0900

    react query develtools

--- src/routes/Coin.tsx
+++ src/routes/Coin.tsx
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useQuery } from "react-query";
+import { Switch, Route, Link, useLocation, useParams, useRouteMatch } from "react-router-dom";
 import styled from "styled-components";
+import Charts from "../routes/Chart";
+import Price  from "../routes/Price";
+import { fetchCoinInfo, fetchCoinTickers } from "./api";
 
 const Container = styled.div`
-    padding: 0px 20px;
+    padding: 0 300px;
 `;
 
 const Header = styled.header`
@@ -25,6 +29,50 @@ const Title = styled.h1 `
     font-size: 48px;
 `;
 
+const Overview = styled.div`
+    display: flex;
+    justify-content: space-between;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 10px 20px;
+    border-radius: 10px;
+`;
+
+const OverviewItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    span:first-child {
+        font-size: 10px;
+        font-weight: 400;
+        text-transform: uppercase;
+        margin-bottom: 5px;
+    };
+`;
+
+const Description = styled.p`
+    margin: 20px 0px;
+`;
+
+const Tabs = styled.div`
+    display: grid;
+    grid-template-columns: repeat(2, 1fr);
+    margin: 25px 0px;
+    gap: 10px;
+`;
+
+const Tab = styled.span<{isActive: boolean}>`
+    text-align: center;
+    text-transform: uppercase;
+    font-size: 12px;
+    font-weight: 400;
+    background-color: rgba(0, 0, 0, 0.5);
+    padding: 7px 0px;
+    border-radius: 10px;
+    color: ${props => props.isActive ? props.theme.accentColor : props.theme.textColor};
+    a {
+        display: block;
+    };
+`;
 
 interface RouteParams {
     coinId: string;
@@ -33,35 +81,158 @@ interface RouteState {
     name: string;
 }
 
+interface ITag {
+    coin_counter: number;
+    ico_counter: number;
+    id: string;
+    name: string;
+}
+
+interface IInfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface IPriceData {
+    id : string;
+    name : string;
+    symbol : string;
+    rank : number;
+    circulating_supply : number;
+    total_supply : number;
+    max_supply : number;
+    beta_value : number;
+    first_data_at : string;
+    last_updated : string;
+    quotes : {
+        USD:{
+            ath_date: string;
+            ath_price: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_1h: number;
+            percent_change_1y: number;
+            percent_change_6h: number;
+            percent_change_7d: number;
+            percent_change_12h: number;
+            percent_change_15m: number;
+            percent_change_24h: number;
+            percent_change_30d: number;
+            percent_change_30m: number;
+            percent_from_price_ath: number;
+            price: number;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+        }
+    };
+}
+
 function Coin() {
-    const [ loading, setLoading ] = useState(true);
     const { coinId } = useParams<RouteParams>();
     const { state } = useLocation<RouteState>();
-    const [ info, setInfo ] = useState({});
-    const [ priceInfo, setPriceInfo ] = useState({});
+    const priceMatch = useRouteMatch("/:coinId/price");
+    const chartMatch = useRouteMatch("/:coinId/chart");
+    const {isLoading: infoLoading, data: infoData} = useQuery<IInfoData>(["info", coinId], () => fetchCoinInfo(coinId));
+    const {isLoading: tickersLoading, data: tickersData} = useQuery<IPriceData>(["tickers", coinId], () => fetchCoinTickers(coinId));
+    
+    const loading = infoLoading || tickersLoading;
 
+
+    /*
+    const [ loading, setLoading ] = useState(true);
+    const [ info, setInfo ] = useState<IInfoData>();
+    const [ priceInfo, setPriceInfo ] = useState<IPriceData>();
     useEffect(() => {
         (async () => {
             const infoData = await (
                 await fetch(`https://api.coinpaprika.com/v1/coins/${coinId}`)
             ).json();
+            console.log(infoData)
             const priceData = await (
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
+            console.log(priceData)
             setInfo(infoData);
             setPriceInfo(priceData);
+            setLoading(false);
         })();
-    }, []);
-
+    }, [coinId]);
+*/
     return (
         <Container>
             <Header>
-                <Title>{state?.name || "Loading ..."}</Title>
+                <Title>{state?.name ? state.name : loading ? "Loading ..." : infoData?.name}</Title>
             </Header>
             {
                 loading ? (
                     <Loader>"Loading ..."</Loader>
-                ) : null
+                ) : (
+                    <Container>
+                        <Overview>
+                            <OverviewItem>
+                                <span>Rank:</span>
+                                <span>{infoData?.rank}</span>
+                            </OverviewItem>
+                            <OverviewItem>
+                                <span>Symbol:</span>
+                                <span>{infoData?.symbol}</span>
+                            </OverviewItem>
+                            <OverviewItem>
+                                <span>Open Source:</span>
+                                <span>{infoData?.open_source ? "YES" : "NO"}</span>
+                            </OverviewItem>
+                        </Overview>
+                        <Description>
+                            {infoData?.description}
+                        </Description>
+                        <Overview>
+                            <OverviewItem>
+                                <span>Total Suply:</span>
+                                <span>{tickersData?.total_supply}</span>
+                            </OverviewItem>
+                            <OverviewItem>
+                                <span>Max Suply:</span>
+                                <span>{tickersData?.max_supply}</span>
+                            </OverviewItem>
+                        </Overview>
+                        <Tabs>
+                            <Tab isActive={priceMatch !== null}>
+                                <Link to={`/${coinId}/price`}>
+                                    Price
+                                </Link>
+                            </Tab>
+                            <Tab isActive={chartMatch !== null}>
+                                <Link to={`/${coinId}/chart`}>
+                                    Chart
+                                </Link>
+                            </Tab>
+                        </Tabs>
+                        <Switch>
+                            <Route path={`/:coinId/price`} >
+                                <Price />
+                            </Route>
+                            <Route path={`/:coinId/chart`}>
+                                <Charts />
+                            </Route>
+                        </Switch>
+                    </Container>
+                )
             }
         </Container>   
     );
--- src/routes/Coins.tsx
+++ src/routes/Coins.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from "react";
+import { useQuery } from "react-query";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import { fetchCoins } from "./api";
 
 const Container = styled.div`
     padding: 0px 20px;
@@ -70,18 +72,7 @@ interface CoinInterface {
 }
 
 function Coins() {
-    const [ coins, setCoins ] = useState<CoinInterface[]>([]);
-    const [ loading, setLoading ] = useState(true);
-
-    useEffect(() => {
-        (async () => {
-            const response = await fetch("https://api.coinpaprika.com/v1/coins");
-            const json = await response.json();
-            setCoins(json.slice(0, 100));
-            setLoading(false);
-            console.log("useEffect loading state : ", loading);
-        })();
-    });
+    const { isLoading, data } = useQuery<CoinInterface[]>("allCoins", fetchCoins);
 
     return (
         <Container>
@@ -90,12 +81,12 @@ function Coins() {
             </Header>
             <Main>
                 {
-                    loading ? (
+                    isLoading ? (
                         <Loader>"Loading ..."</Loader>
                     ): (
 
                         <ConinsList>                
-                            {coins.map(coin => (
+                            {data?.slice(0, 100).map(coin => (
                                 <Coin key={coin.id}>
                                     <Link to={{
                                             pathname: `/${coin.id}`,
